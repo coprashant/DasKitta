cdfa248ed2a6abe2c00226fb772ee889f9fcf77e
Prasant Bhattarai <bhattaraiprashant010@gmail.com>
2026-03-13 19:35:49 +0545

    deleted the node modules

--- backend/src/routes/accounts.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addAccount, getAccounts } = require('../controllers/accountController');
-
-router.post('/', addAccount);
-router.get('/', getAccounts);
-
-module.exports = router;
--- backend/src/routes/ipo.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { bulkApply } = require('../controllers/ipoController');
-
-// Route to trigger bulk application
-router.post('/apply-ipo', bulkApply);
-
-module.exports = router;
